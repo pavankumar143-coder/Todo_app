7226c5f5023b0c63cdeaf2fab71dc1daa80a8b46
pavankumar <pavankumartule143@gmail.com>
2023-02-07 17:01:58 +0530

    createdUrlLoginView

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,9 +1,10 @@
 # from django.conf.urls import include
-from .views import SignUpView
+from .views import SignUpView,LoginView
 from django.urls import path
 
 urlpatterns = [
     path('signup/',SignUpView.as_view(),name='signup'),
-
+    path('login/',LoginView.as_view(),name='login'),
+   
 ]
 
