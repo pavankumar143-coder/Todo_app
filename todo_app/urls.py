65e6c766c7349f7ceb6bc285888bcc114b70c569
pavankumar <pavankumartule143@gmail.com>
2023-02-07 16:57:08 +0530

    createdUrlForSignupView

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,3 +1,9 @@
 # from django.conf.urls import include
+from .views import SignUpView
+from django.urls import path
 
+urlpatterns = [
+    path('signup/',SignUpView.as_view(),name='signup'),
+
+]
 
