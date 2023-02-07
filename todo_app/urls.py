92a261b60f1c76c2bb1a54a0440229a2a39e8ee7
pavankumar <pavankumartule143@gmail.com>
2023-02-07 17:07:34 +0530

    createdUrlTodoRetrieveUpdateDestroyView

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,11 +1,11 @@
 # from django.conf.urls import include
-from .views import SignUpView,LoginView,TodoListCreateView
+from .views import SignUpView,LoginView,TodoListCreateView,TodoRetrieveUpdateDestroyView
 from django.urls import path
 
 urlpatterns = [
     path('signup/',SignUpView.as_view(),name='signup'),
     path('login/',LoginView.as_view(),name='login'),
     path('todos/',TodoListCreateView.as_view(),name='todoslist'),
-
+    path('todosupdate/<int:pk>',TodoRetrieveUpdateDestroyView.as_view(),name='todosupdate')
 ]
 
