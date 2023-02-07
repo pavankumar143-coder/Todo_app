c5f763b6478b10a5593d1537c570f265b355d8f2
pavankumar <pavankumartule143@gmail.com>
2023-02-07 17:05:22 +0530

    createdUrlTodoListCreateView

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,10 +1,11 @@
 # from django.conf.urls import include
-from .views import SignUpView,LoginView
+from .views import SignUpView,LoginView,TodoListCreateView
 from django.urls import path
 
 urlpatterns = [
     path('signup/',SignUpView.as_view(),name='signup'),
     path('login/',LoginView.as_view(),name='login'),
-   
+    path('todos/',TodoListCreateView.as_view(),name='todoslist'),
+
 ]
 
