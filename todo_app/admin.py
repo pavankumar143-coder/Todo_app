1d25506b6628b63543463faab5eb13d705af1591
pavankumar <pavankumartule143@gmail.com>
2023-02-07 17:11:54 +0530

    createdadminAndRegisterTodoAndUser

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,4 +1,11 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import User,Todo
+
+class Usercreate(admin.ModelAdmin):
+    list_display = ['id','username','mobile','password']
+    class Todoscreate(admin.ModelAdmin):
+     list_display = ['id','owner','task','due_date','completed']
+    admin.site.register(Todo,Todoscreate)
+admin.site.register(User,Usercreate,)
 
