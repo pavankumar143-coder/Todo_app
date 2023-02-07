105d0e5a91de1727a154d8e4d4190356f3d69cc8
pavankumar <pavankumartule143@gmail.com>
2023-02-07 16:54:47 +0530

    createdsignupview

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render
+from .models import User,Todo
+from .Serializer import UserSerializer
+from rest_framework import generics
+from rest_framework.response import Response
 # Create your views here.
+
+class SignUpView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+
+
 
